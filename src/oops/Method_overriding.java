975549f1634ef4bb1abef1e676f25280a17d1606
SakthipriyaAnand <bsakthipriya12793@gmail.com>
2023-09-26 08:07:19 +0530

    updated

--- src/oops/Method_overriding.java
+++ src/oops/Method_overriding.java
@@ -3,12 +3,15 @@ class A{
 	public void method1() {
 		System.out.println("class A");
 	}
+	public static void method4() {
+		System.out.println("static method in class A");
+	}
 }
 
 class B extends A{
 	public void method2() {
 		System.out.println("class B");
-		super.method1();
+		//super.method1();
 	}
 	public static void method3() {
 		System.out.println("class c");
@@ -16,7 +19,10 @@ class B extends A{
 	@Override
 	public void method1() {
 		System.out.println("class A1");
-		this.method2();
+		//this.method2();
+	}
+	public static void method4() {
+		System.out.println("static method in class B");
 	}
 }
 public class Method_overriding extends  B{
@@ -24,11 +30,12 @@ public class Method_overriding extends  B{
 	public static void main(String[] args) {
 		
 		Method_overriding m=new Method_overriding();
-		m.method1();
-		m.method2();
-		//m.method3();
-		//B.method3();
-		
+		m.method1();//non static overridden method 
+		m.method2();//non static method in class B
+		//m.method3();// static methods cannot be overridden
+		B.method3();// static methods cannot be overridden
+		B.method4();//static method in class A
+		A.method4();//static method in class B
 	}
 
 }
